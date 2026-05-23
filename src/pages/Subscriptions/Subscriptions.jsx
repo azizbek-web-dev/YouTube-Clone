1cef1cd4c750f0fbf0c2fd028fb72be319834bcd
Azizbek Hakimov <azizxakimov45@gmail.com>
2026-05-24 01:54:55 +0500

    refactor subscriptions to use empty state

--- src/pages/Subscriptions/Subscriptions.jsx
+++ src/pages/Subscriptions/Subscriptions.jsx
@@ -1,23 +1,13 @@
 import { MdSubscriptions } from 'react-icons/md'
-import { FiUser } from 'react-icons/fi'
-import './Subscriptions.css'
+import EmptyState from '../../components/EmptyState/EmptyState.jsx'
 
 function Subscriptions() {
   return (
-    <div className="subs-page">
-      <div className="subs-empty">
-        <MdSubscriptions className="subs-empty-icon" />
-        <h2 className="subs-empty-title">Yangi videolarni o'tkazib yubormang</h2>
-        <p className="subs-empty-text">
-          Sevimli YouTube kanallaringizdan yangiliklarni ko'rish uchun hisobga
-          kiring
-        </p>
-        <button className="subs-signin-btn">
-          <FiUser />
-          <span>Kirish</span>
-        </button>
-      </div>
-    </div>
+    <EmptyState
+      icon={<MdSubscriptions />}
+      title="Yangi videolarni o'tkazib yubormang"
+      text="Sevimli YouTube kanallaringizdan yangiliklarni ko'rish uchun hisobga kiring"
+    />
   )
 }
 
